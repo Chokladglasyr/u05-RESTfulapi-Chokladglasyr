6dc61f792f224528c08ece3a3e7ef7f2d71d504b
Chokladglasyr <lim_ida@live.com>
2025-03-07 12:49:36 +0100

    feat: added interface and model without mongoDB

--- models/userModel.ts
+++ models/userModel.ts
@@ -1,8 +1,28 @@
-import mongoose from "mongoose";
+import User from "../interface/userInterface"
+// import mongoose from "mongoose";
 
 
-const userSchema = new mongoose.Schema({name: String, email: String, password: String, confirmed_password: String}, {collection: 'users'});
+// const userSchema = new mongoose.Schema({name: String, email: String, password: String, confirmed_password: String}, {collection: 'users'});
 
-const User = mongoose.model('User', userSchema);
+// const User = mongoose.model('User', userSchema);
 
-export default User;
+// export default User;
+
+let users: User[] = [
+    {
+        "id": 1,
+        "name": "ida",
+        "email": "ida@example.com",
+        "password": "test1234",
+        "confirmed_password": "test1234"
+    },
+    {
+        "id": 2,
+        "name": "aylin",
+        "email": "aylin@example.com",
+        "password": "test5678",
+        "confirmed_password": "test5678"
+    }
+];
+
+export default users;
