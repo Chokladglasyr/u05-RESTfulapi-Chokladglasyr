1d86db4e3fd6a9c7c6d4e569123b462541b94cc8
Chokladglasyr <lim_ida@live.com>
2025-03-07 12:59:45 +0100

    feat: added getUsers, getUserByName, createUser

---
+++ controllers/userController.ts
@@ -0,0 +1,26 @@
+import {Request, Response } from "express";
+import users from "../models/userModel"
+
+
+const getUsers = (req: Request, res: Response) => {
+    res.json(users);
+};
+
+const getUserByName = (req: Request, res: Response) => {
+    const user = users.find((u) => u.name == req.params.name);
+    if (!user) {
+        res.status(404).json({message: "User not found"});
+        return;
+    }
+    res.json(user);
+};
+
+const createUser = (req: Request, res: Response) => {
+    const newUser = {
+        id: users.length + 1,
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        confirmed_password: req.body.confirmed_password,
+    }
+}
