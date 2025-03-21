3e78d9f26e4e394ef5b4d9bf519cfa28759158c1
Chokladglasyr <lim_ida@live.com>
2025-03-21 16:47:02 +0100

    feat: added updateList_item

--- controllers/list_itemsController.ts
+++ controllers/list_itemsController.ts
@@ -69,6 +69,21 @@ export const createList_item = async (req: Request, res: Response) => {
         }
     }  
 }
+export const updateList_item = async (req: Request, res: Response) => {
+    try {
+        const item = await List_item.findByIdAndUpdate(req.params.id, req.body, {new: true});
+        if(!item) {
+            res.status(404).json({message: "Can't find item."})
+        }
+        res.json({message: "Item updated!", item});
+
+    } catch(error: unknown) {
+        if (error instanceof Error) {
+            res.status(500).json({error: error.message});
+            return;
+        }
+    }  
+}
 // try {
 
 // } catch(error: unknown) {
@@ -78,49 +93,6 @@ export const createList_item = async (req: Request, res: Response) => {
 //     }
 // }
 
-// export const createList_item = (req: Request, res: Response) => {
-//     const list = lists.find((l) => l.id === parseInt(req.params.id));
-//     if (!list) {
-//         res.status(404).json({message: "Cannot find list"});
-//     }
-//     const { link, description, photo, price} = req.body;
-//     const newItem = {
-//         id: list_items.length + 1,
-//         listId: parseInt(req.params.id),
-//         link: link,
-//         description: description,
-//         photo: photo,
-//         price: parseInt(price) || 0,
-//     };
-//     list_items.push(newItem);
-//     const items = list_items.filter((i) => i.listId === parseInt(req.params.id));
-
-//     res.status(201).json(items);
-// };
-// export const updateList_item = (req: Request, res: Response) => {
-//     const {name} = req.query;
-//     const list_itemIndex = list_items.findIndex((i) => (i.id) === parseInt(req.params.id));
-//     const { link, description, photo, price } = req.body;
-//     const user = users.find((u) => u.name === name);
-
-//     if(!user || user.name != lists[list_itemIndex].username) {
-//         res.status(404).json({message: "Access denied"});
-//         return;
-//     }
-
-//     if (list_itemIndex === -1) {
-//         res.status(404).json({message: "Item not found"});
-//         return;
-//     }
-//     list_items[list_itemIndex] = {... list_items[list_itemIndex],
-//         link: link ?? list_items[list_itemIndex].link,
-//         description: description ?? list_items[list_itemIndex].description,
-//         photo: photo ?? list_items[list_itemIndex].photo,
-//         price: price ?? list_items[list_itemIndex].price,
-//     };
-//     res.json({message: "List item updated", list_item: list_items[list_itemIndex]});
-    
-// };
 // export const deleteList_item = (req: Request, res: Response) => {
 //     const {name} = req.query;
 //     const list_itemIndex = lists.findIndex((i) => (i.id) === parseInt(req.params.id));
--- routes/list_itemRoutes.ts
+++ routes/list_itemRoutes.ts
@@ -1,5 +1,5 @@
 import express, {Request, Response} from "express";
-import { createList_item, getList_items, getList_itemsByList, getList_itemsByUser } from "../controllers/list_itemsController";
+import { createList_item, getList_items, getList_itemsByList, getList_itemsByUser, updateList_item } from "../controllers/list_itemsController";
 
 
 const list_itemRouter = express.Router();
@@ -8,7 +8,7 @@ list_itemRouter.get('/', getList_items);
 list_itemRouter.get('/user/:id', getList_itemsByUser);
 list_itemRouter.get('/:id', getList_itemsByList);
 list_itemRouter.post('/:id', createList_item);
-// list_itemRouter.put('/:id', updateList_item);
+list_itemRouter.put('/:id', updateList_item);
 // list_itemRouter.delete('/:id', deleteList_item);
 
 export default list_itemRouter;
