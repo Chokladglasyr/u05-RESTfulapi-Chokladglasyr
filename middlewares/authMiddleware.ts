35e33caf42b9ca4c61ce8a0bd4e56e6858297dc0
Chokladglasyr <lim_ida@live.com>
2025-04-16 14:09:53 +0200

    feat:readded the bearer token bit

--- middlewares/authMiddleware.ts
+++ middlewares/authMiddleware.ts
@@ -8,13 +8,13 @@ import List_item from "../models/list_itemsModel";
 export const authCheck = async (req: AuthRequest, res: Response, next: NextFunction) => {
     const authHeader = req.header("Authorization")
 
-    if (!authHeader) {
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
         res.status(401).json({message: "You need to log in to be able to do that"});
         return;
     }
 
-    const token = authHeader;
-    
+    const token = authHeader.split(" ")[1];
+ 
 
     if (!token) {
         res.status(401).json({message: "No Access"});
